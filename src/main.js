94adbbafd35cac83cd9ea45760d7bf891918ee4a
robertobirijnr <robertobirijnr@hotmail.com>
2020-11-16 16:56:36 +0000

    Appbar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+
+// Import the styles directly. (Or you could add them via script tags.)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import 'font-awesome/css/font-awesome.min.css';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
