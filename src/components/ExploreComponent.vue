ff6dbf7d2525cec41c41fd4f57dfcbe29982a147
robertobirijnr <robertobirijnr@hotmail.com>
2020-11-17 04:05:54 +0000

    explore section

---
+++ src/components/ExploreComponent.vue
@@ -0,0 +1,66 @@
+<template>
+   <div>
+        <section id="explore-head-section">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center py-5">
+                     <h1 class="display-4">
+                         Explore
+                     </h1>
+                     <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, repellendus. Quos, laboriosam deserunt, impedit quis iure illum vero voluptates reiciendis facere perspiciatis, voluptatum rerum commodi. Quasi dolore reprehenderit reiciendis facere aperiam architecto, recusandae distinctio? Cumque vero eos facere perspiciatis et maxime, numquam fuga pariatur quidem amet provident ea suscipit veniam?</p>
+                     <a href="#" class="btn btn-outline-secondary">Find out more</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="explore-section" class="bg-light text-muted py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="../assets/explore-section1.jpg" alt="" class="img-fluid mb-3">
+                </div>
+                <div class="col-md-6">
+                    <h1>Explore & Connect</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum nesciunt qui excepturi fugiat ut sit rerum consequuntur, vero dolor delectus!</p>
+                    <div class="d-flex">
+                        <div class="p-4 align-self-start">
+                            <i class="fa fa-check fa-2x"></i>
+                        </div>
+                        <div class="p-4 align-self-end">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis earum eveniet, sed nemo voluptas ipsum!</p>
+                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <div class="p-4 align-self-start">
+                            <i class="fa fa-check fa-2x"></i>
+                        </div>
+                        <div class="p-4 align-self-end">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis earum eveniet, sed nemo voluptas ipsum!</p>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    </section>
+   </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style  scoped>
+#explore-head-section{
+    background: #333;
+    color:#fff
+  }
+
+  #explore-section .fa, #share-section .fa{
+      background: #333;
+      color: #fff;
+      padding: 5px;
+      border-radius: 5px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <appbar/>
     <Hero/>
+    <explore-component/>
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 // @ is an alias to /src
 import Appbar from '@/components/Appbar'
 import Hero from '@/components/HeroComponent'
+import ExploreComponent from '../components/ExploreComponent.vue'
 
 export default {
   name: 'Home',
   components: {
     Appbar,
-    Hero
+    Hero,
+    ExploreComponent
   }
 }
 </script>
