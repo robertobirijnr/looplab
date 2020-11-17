4ee1716ad0d4af3264a97b4252077479cec830df
robertobirijnr <robertobirijnr@hotmail.com>
2020-11-17 04:35:08 +0000

    share and create component

---
+++ src/components/CreateComponent.vue
@@ -0,0 +1,70 @@
+<template>
+   <div>
+        <section id="create-head-section" class="bg-primary text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center py-5">
+                     <h1 class="display-4">
+                        Create
+                     </h1>
+                     <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, repellendus. Quos, laboriosam deserunt, impedit quis iure illum vero voluptates reiciendis facere perspiciatis, voluptatum rerum commodi. Quasi dolore reprehenderit reiciendis facere aperiam architecto, recusandae distinctio? Cumque vero eos facere perspiciatis et maxime, numquam fuga pariatur quidem amet provident ea suscipit veniam?</p>
+                     <a href="#" class="btn btn-outline-light">Find out more</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="create-section" class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 order-2">
+                    <img src="../assets/create-section1.jpg" alt="" class="img-fluid mb-3">
+                </div>
+                <div class="col-md-6 order-1">
+                    <h1>Create Your Passion</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum nesciunt qui excepturi fugiat ut sit rerum consequuntur, vero dolor delectus!</p>
+                    <div class="d-flex">
+                        <div class="p-4 align-self-start">
+                            <i class="fa fa-check fa-2x"></i>
+                        </div>
+                        <div class="p-4 align-self-end">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis earum eveniet, sed nemo voluptas ipsum!</p>
+                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <div class="p-4 align-self-start">
+                            <i class="fa fa-check fa-2x"></i>
+                        </div>
+                        <div class="p-4 align-self-end">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis earum eveniet, sed nemo voluptas ipsum!</p>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    </section>
+   </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style  scoped>
+
+
+  #explore-section .fa, #share-section .fa{
+      background: #333;
+      color: #fff;
+      padding: 5px;
+      border-radius: 5px;
+  }
+
+ #create-section .fa{
+    color: #fff;
+    background: #333;
+    padding: 5px;
+    border-radius:5px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@
     <appbar/>
     <Hero/>
     <explore-component/>
+    <create-component/>
+    <share-component/>
   </div>
 </template>
 
@@ -11,13 +13,17 @@
 import Appbar from '@/components/Appbar'
 import Hero from '@/components/HeroComponent'
 import ExploreComponent from '../components/ExploreComponent.vue'
+import CreateComponent from '../components/CreateComponent.vue'
+import ShareComponent from '../components/ShareComponent.vue'
 
 export default {
   name: 'Home',
   components: {
     Appbar,
     Hero,
-    ExploreComponent
+    ExploreComponent,
+    CreateComponent,
+    ShareComponent
   }
 }
 </script>
